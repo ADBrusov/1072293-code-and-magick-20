d1f7a361684b3f95728845b9f8670fae563160e0
Alexander <ADBrusob96@yandex.ru>
2020-06-23 15:28:28 +0300

    Разбивает код на отдельные логические функции

--- js/stat.js
+++ js/stat.js
@@ -16,6 +16,16 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
+var renderText = function (ctx, x, y, text, color, fontSize, fontFamily) {
+  ctx.fillStyle = color;
+  ctx.font = fontSize + fontFamily;
+  ctx.fillText(text, x, y);
+};
+
+var renderBar = function (ctx, x, y, width, height) {
+  ctx.fillRect(x, y, width, height);
+};
+
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
   for (var i = 1; i < arr.length; i++) {
@@ -32,27 +42,21 @@ var getRandomPercent = function () {
 };
 
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)'); // Рендерим тень облака
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff'); // Рендерим облако
+  renderText(ctx, CLOUD_X + TEXT_GAP, CLOUD_Y + TEXT_GAP * 2, 'Ура вы победили!', '#000', '16px', 'PT Mono'); // Рендерим подпись для статистики
+  renderText(ctx, CLOUD_X + TEXT_GAP, CLOUD_Y + TEXT_GAP * 3, 'Список результатов:', '#000', '16px', 'PT Mono'); // Рендерим подпись для статистики
 
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', CLOUD_X + TEXT_GAP, CLOUD_Y + TEXT_GAP * 2);
-  ctx.fillText('Список результатов:', CLOUD_X + TEXT_GAP, CLOUD_Y + TEXT_GAP * 3);
-
-  var maxTime = getMaxElement(times);
+  var maxTime = getMaxElement(times); // Находим максимальный элемент в массиве times
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillStyle = '#000';
-    ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - TEXT_GAP);
+    renderText(ctx, CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - TEXT_GAP, players[i], '#000'); // Рендерим текст с именами игроков из массива players
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = 'hsl(240, ' + getRandomPercent() + ', 50%)';
     }
-    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - TEXT_GAP - FONT_GAP - (TEXT_GAP / 2) - (BAR_HEIGHT * times[i]) / maxTime, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
-
-    ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - TEXT_GAP - FONT_GAP - (TEXT_GAP / 2) - (BAR_HEIGHT * times[i]) / maxTime - (TEXT_GAP / 2));
+    renderBar(ctx, CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - TEXT_GAP - FONT_GAP - (TEXT_GAP / 2) - (BAR_HEIGHT * times[i]) / maxTime, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime); // Рендерим столбики статистики
+    renderText(ctx, CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - TEXT_GAP - FONT_GAP - (TEXT_GAP / 2) - (BAR_HEIGHT * times[i]) / maxTime - (TEXT_GAP / 2), Math.round(times[i]), '#000'); // Рендерим текст со значением времени прохождения каждого игрока
   }
 };
